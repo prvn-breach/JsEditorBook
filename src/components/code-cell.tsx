0084dc4745e22503f22b1ff7437a2cdbeaadca47
prvn-breach <praveenking880@gmail.com>
2021-03-28 19:03:27 +0530

    changes

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -6,12 +6,14 @@ import Resizable from "../components/resizable";
 
 const CodeCell = () => {
     const [input, setInput] = useState('');
+    const [err, setErr] = useState('');
     const [code, setCode] = useState('');
 
     useEffect(() => {
         const timer = setTimeout(async () => {
             const output = await bundle(input);
-            setCode(output);
+            setCode(output.code);
+            setErr(output.err);
         }, 1000);
 
         return () => {
@@ -27,7 +29,7 @@ const CodeCell = () => {
                 </Resizable>
                 {/* <textarea value={input} onChange={(e) => setInput(e.target.value)} /> */}
                 {/* <div><button onClick={onClick}>Submit</button></div> */}
-                <Preview code={code} />
+                <Preview code={code} err={err} />
             </div>
         </Resizable>
     );
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 
 interface PreviewProps {
     code: string;
+    err: string;
 };
 
 const html = `
@@ -17,13 +18,22 @@ const html = `
     <body>
         <div id="root"></div>
         <script>
+            const handleError = (err) => {
+                const root = document.querySelector("#root");
+                root.innerHTML = '<div style="color: red;"><h4>RunTime Error</h4>' + err + '</div>';
+                console.error(err);
+            };
+
+            window.addEventListener('error', (event) => {
+                event.preventDefault();
+                handleError(event.error);   
+            });
+
             window.addEventListener('message', (event) => {
                 try {
                     eval(event.data);
                 } catch (err) {
-                    const root = document.querySelector("#root");
-                    root.innerHTML = '<div style="color: red;"><h4>RunTime Error</h4>' + err + '</div>';
-                    console.error(err);
+                    handleError(err);
                 }
             }, false);
         </script>
@@ -31,7 +41,7 @@ const html = `
 </html>
 `;
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
     const iframeRef = useRef<any>();
 
     useEffect(() => {
@@ -41,12 +51,13 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
     return (
         <div className="preview-wrapper">
-            <iframe 
-                title="preview" 
-                ref={iframeRef} 
-                sandbox="allow-scripts" 
-                srcDoc={html} 
+            <iframe
+                title="preview"
+                ref={iframeRef}
+                sandbox="allow-scripts"
+                srcDoc={html}
             />
+            {err && <div className="preview-error">{err}</div>}
         </div>
     );
 };
