e431505777246f3eb74c44d1b297eabae5efd846
prvn-breach <praveenking880@gmail.com>
2021-03-29 20:09:48 +0530

    add lot of styles and redux store

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -3,15 +3,21 @@ import CodeEditor from "./code-editor";
 import Preview from "./preview";
 import bundle from "../bundler";
 import Resizable from "../components/resizable";
+import { Cell } from "../state";
+import { useActions } from "../hooks/use-actions";
 
-const CodeCell = () => {
-    const [input, setInput] = useState('');
+interface CodeCellProps {
+    cell: Cell
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const [err, setErr] = useState('');
     const [code, setCode] = useState('');
+    const { updateCell } = useActions();
 
     useEffect(() => {
         const timer = setTimeout(async () => {
-            const output = await bundle(input);
+            const output = await bundle(cell.content);
             setCode(output.code);
             setErr(output.err);
         }, 1000);
@@ -19,13 +25,13 @@ const CodeCell = () => {
         return () => {
             clearTimeout(timer);
         };
-    }, [input]);
+    }, [cell.content]);
 
     return (
         <Resizable direction="vertical">
-            <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+            <div style={{ height: "calc(100% - 10px)", display: "flex", flexDirection: "row" }}>
                 <Resizable direction="horizontal">
-                    <CodeEditor onChange={(value) => setInput(value)} initialValue="const a = 1" />
+                    <CodeEditor onChange={(value) => updateCell(cell.id, value)} initialValue="const a = 1" />
                 </Resizable>
                 {/* <textarea value={input} onChange={(e) => setInput(e.target.value)} /> */}
                 {/* <div><button onClick={onClick}>Submit</button></div> */}
